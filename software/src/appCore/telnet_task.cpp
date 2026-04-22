b30a9883290f07e41075acb3a25db70724d97162
lamiika <47070356+lamiika@users.noreply.github.com>
2026-04-22 17:10:42 +0300

    Fixed telnet crash

--- software/src/appCore/telnet_task.cpp
+++ software/src/appCore/telnet_task.cpp
@@ -10,6 +10,9 @@
 
 #include "AsyncTCP.h"
 #include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
 
 /////////////////////////////// 2.Macros ///////////////////////////////
 /////////////////////////////// 3.Types ////////////////////////////////
@@ -49,4 +52,19 @@ public:
   }
 };
 
-static TelnetHandler telnet;
+static TelnetHandler* telnet = nullptr;
+
+void telnet_start() {
+  telnet = new TelnetHandler();
+}
+
+void wifi_init()
+{  
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
---
+++ software/src/appCore/telnet_task.h
@@ -0,0 +1,8 @@
+#ifndef AFE_CORE_H
+#define AFE_CORE_H
+
+void telnet_start();
+
+void wifi_init();
+
+#endif
